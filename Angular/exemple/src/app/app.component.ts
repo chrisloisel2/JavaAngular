fab8dd2503fe55ac64c9d563f441055d579963b6
ChristopherLoisel <christo.loisel@gmail.com>
2024-12-04 14:57:13 +0100

    ok

--- Angular/exemple/src/app/app.component.ts
+++ Angular/exemple/src/app/app.component.ts
@@ -16,24 +16,38 @@ export class AppComponent {
 	// Attribut
 	// Dictionary
 	// user.nom
+
+	amis: any = []
+
 	users: any = [{
 		age: 25,
 		nom: 'Loisel',
+		connected: true,
 		prenom: 'Christopher',
 		pp: 'https://media.licdn.com/dms/image/v2/D4D03AQHGrcYsIRi__w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1698420713920?e=1738800000&v=beta&t=v5VJjtiA5EHNO7RwtAk2LqlDCY2zyGktKp0d2Aa4OH0'
 	},
 	{
 		age: 35,
 		nom: "Doe",
+		connected: true,
 		prenom: "John",
 		pp: "https://thispersondoesnotexist.com/",
 	},
 	{
 		age: 45,
 		nom: "Travolta",
+		connected: true,
 		prenom: "John",
 		pp: "https://thispersondoesnotexist.com/",
 	},
 
 	];
+
+
+	// Fonction
+	addFriend() {
+		// this.amis.push(user);
+
+		console.log('La fonction addFriend a été appellée');
+	}
 }
--- Angular/exemple/src/app/components/user/user.component.ts
+++ Angular/exemple/src/app/components/user/user.component.ts
@@ -1,15 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
 	selector: 'app-user',
-	imports: [],
+	imports: [CommonModule],
 	templateUrl: './user.component.html',
 	styleUrl: './user.component.css'
 })
 export class UserComponent {
 
+	@Output("add")
+	add: EventEmitter<any> = new EventEmitter();
+
 	@Input()
 	user: any;
 	// Variable utilisateur, type any, attendue en entrée
+
+	constructor() {
+		// Constructeur
+	}
+
+	connect() {
+		// Fonction connect
+		this.user.connected = true;
+	}
+
+	disconnect() {
+		// Fonction disconnect
+		this.user.connected = false;
+	}
+
+	emitAdd() {
+		this.add.emit();
+	}
 
 }
