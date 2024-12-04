5263ab400a49ff5f2ce67614b2e3871624b287b3
ChristopherLoisel <christo.loisel@gmail.com>
2024-12-04 12:30:04 +0100

    ok

--- Angular/exemple/src/app/app.component.ts
+++ Angular/exemple/src/app/app.component.ts
@@ -13,6 +13,22 @@ import { UserComponent } from './components/user/user.component';
 })
 export class AppComponent {
 	// Attribut
-	nom: string = 'Loisel';
-	prenom: string = 'Romain';
+	// Dictionary
+	// user.nom
+	currentuser: any = [{
+		nom: 'Loisel',
+		prenom: 'Christopher',
+		pp: 'https://media.licdn.com/dms/image/v2/D4D03AQHGrcYsIRi__w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1698420713920?e=1738800000&v=beta&t=v5VJjtiA5EHNO7RwtAk2LqlDCY2zyGktKp0d2Aa4OH0'
+	},
+	{
+		nom: "Doe",
+		prenom: "John",
+		pp: "https://thispersondoesnotexist.com/",
+	},
+	{
+		nom: "Travolta",
+		prenom: "John",
+		pp: "https://thispersondoesnotexist.com/",
+	},
+	];
 }
--- Angular/exemple/src/app/components/user/user.component.ts
+++ Angular/exemple/src/app/components/user/user.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
-  imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+	selector: 'app-user',
+	imports: [],
+	templateUrl: './user.component.html',
+	styleUrl: './user.component.css'
 })
 export class UserComponent {
+
+	@Input()
+	user: any;
+	// Variable utilisateur, type any, attendue en entrée
 
 }
