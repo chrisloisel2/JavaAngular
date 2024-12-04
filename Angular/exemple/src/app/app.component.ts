11220562a8371f5b5bfcbbd0dea18c43cd550f7d
ChristopherLoisel <christo.loisel@gmail.com>
2024-12-04 14:26:35 +0100

    ok

--- Angular/exemple/src/app/app.component.ts
+++ Angular/exemple/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
+import { CommonModule } from '@angular/common';
 
 // Décorateur @Component
 // Permet de définir les métadonnées du composant
 @Component({
 	// Selector: Nom de la balise HTML
 	selector: 'app-root',
-	imports: [RouterOutlet, UserComponent],
+	imports: [RouterOutlet, UserComponent, CommonModule],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css'
 })
@@ -15,20 +16,24 @@ export class AppComponent {
 	// Attribut
 	// Dictionary
 	// user.nom
-	currentuser: any = [{
+	users: any = [{
+		age: 25,
 		nom: 'Loisel',
 		prenom: 'Christopher',
 		pp: 'https://media.licdn.com/dms/image/v2/D4D03AQHGrcYsIRi__w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1698420713920?e=1738800000&v=beta&t=v5VJjtiA5EHNO7RwtAk2LqlDCY2zyGktKp0d2Aa4OH0'
 	},
 	{
+		age: 35,
 		nom: "Doe",
 		prenom: "John",
 		pp: "https://thispersondoesnotexist.com/",
 	},
 	{
+		age: 45,
 		nom: "Travolta",
 		prenom: "John",
 		pp: "https://thispersondoesnotexist.com/",
 	},
+
 	];
 }
