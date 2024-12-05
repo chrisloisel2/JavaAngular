e8ba930cbcc0e10b412a1fd9bfb37c79f783a20f
ChristopherLoisel <christo.loisel@gmail.com>
2024-12-05 14:01:17 +0100

    project messagerie

---
+++ Angular/messagerie/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+
+export const routes: Routes = [
+	{
+		pathMatch: 'full', path: '', redirectTo: 'login'
+	},
+	{
+		path: 'login', component: LoginComponent
+	},
+	{
+		path: 'register', component: RegisterComponent
+	},
+	{
+		path: 'home', component: HomeComponent
+	}
+];
