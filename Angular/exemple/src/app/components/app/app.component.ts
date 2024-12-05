f7c47fa43f36c04f5ff5efb84cf55bd9fbf2b604
ChristopherLoisel <christo.loisel@gmail.com>
2024-12-05 11:29:26 +0100

    ok

--- Angular/exemple/src/app/components/app/app.component.ts
+++ Angular/exemple/src/app/components/app/app.component.ts
@@ -5,17 +5,30 @@ import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user.service';
 import { FormComponent } from '../form/form.component';
 import { LoginComponent } from '../login/login.component';
+import { HttpClient } from '@angular/common/http';
 
 // Décorateur @Component
 // Permet de définir les métadonnées du composant
 @Component({
 	// Selector: Nom de la balise HTML
 	selector: 'app-root',
-	imports: [UserComponent, CommonModule, FormComponent, LoginComponent],
+	imports: [UserComponent, CommonModule, FormComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css'
 })
 export class AppComponent {
-	constructor(public userService: UserService) {
+	constructor(public userService: UserService, public http: HttpClient) {
+	}
+
+
+	apiCall() {
+		let obs = this.http.get("https://api.chucknorris.io/jokes/random")
+
+		obs.subscribe(
+			(response: any) => {
+				console.log(response.value);
+			}
+		);
+
 	}
 }
